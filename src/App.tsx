04765e84de06d7a9d0bbe7f6c5854e86355791d1
Dmitry1on1000000 <KUREVIN.DMITRY@yandex.ru>
2023-01-12 20:01:18 +0300

    Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CounterComponent } from './CounterComponent';
 
 function App() {
+  const handler = () => {
+    console.log('была нажата');
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="discount">-17%</p>
+      <img className="img" src={'/img.jpg'} alt="img" />
+      <button className="btn" onClick={handler}>
+        Быстрый просмотр
+      </button>
+      <h1 className="price">
+        {' '}
+        20 630 руб. <span> 25 000 руб.</span>
+      </h1>
+      <h2 className="discountTwo"> Скидка! </h2>
+      <h3> Компьютер iRU Game 510B5GE, Intel Core i5 10400F,DDR4<br />
+       16ГБ, 1ТБ, <br />240ГБ(SSD), NVIDIA GeForce
+        GTX 1650 - 4096 Мб </h3>
+      <CounterComponent title={'В корзину'}/>
+
     </div>
   );
 }
